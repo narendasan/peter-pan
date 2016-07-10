0ce2ded91f1fd465316746be7355bf61197c0801
Rohan Mathur <rohan@rmathur.com>
2016-07-09 18:30:30 -0700

    Add login to flask app
    Signed-off-by: Rohan Mathur <rohan@rmathur.com>

--- app.py
+++ app.py
@@ -1,36 +1,77 @@
 import os
-from flask import Flask, request, redirect, render_template, url_for
+
+from flask import Flask, flash, request, redirect, render_template, session, abort, url_for
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from werkzeug.utils import secure_filename
+
+from models import User
+
 
 UPLOAD_FOLDER = 'files/uploaded'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+engine = create_engine('sqlite:///users.db', echo=True)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    if not session.get('logged_in'):
+        return render_template('index.html')
+    else:
+        return "Hello Boss!"
+
+@app.route('/login', methods=['GET'])
+def login():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return "Hello Boss!"
+
+@app.route('/do_login', methods=['POST'])
+def do_login():
+    username = str(request.form['username'])
+    password = str(request.form['password'])
+ 
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    query = s.query(User).filter(User.username.in_([username]), User.password.in_([password]) )
+    result = query.first()
+    if result:
+        session['logged_in'] = True
+    else:
+        flash('Unrecognized account! Please try again.')
+    return index()
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/do_logout")
+def logout():
+    session['logged_in'] = False
+    return home()
+
+@app.route('/upload', methods=['POST'])
 def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('upload_file',
-                                    filename=filename))
-    return render_template('index.html')
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit a empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('upload_file',
+                                filename=filename))
 
 if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
     app.run(host='0.0.0.0', port=5001, debug=True)
