f10927a771cd67747d6c9a57a8750f9198f2551f
Aravind Sundaresan <Perilynn@users.noreply.github.com>
2016-07-10 01:31:44 -0700

    Delete image_upscale.py

--- image_upscale.py
+++
@@ -1,4 +0,0 @@
-import cv2
-src = cv2.imread("intro-bgSmall.jpg")
-dest_inter_cubic = cv2.resize(src, None, fx=4, fy=4, interpolation = cv2.INTER_CUBIC)
-cv2.imwrite("intro-bgSmall-upsized-cubic.jpg", dest_inter_cubic)3
